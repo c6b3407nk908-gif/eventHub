aaf55e7b77b4f3a9df7f65fbda2e3117a2857e19
Clive <clive@example.com>
2026-06-10 18:30:38 +0530

    Remove duplicate nested EventMate-main folder

--- EventMate-main/src/main/java/com/eventmate/repository/ApplicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventmate.repository;
-
-import com.eventmate.model.Application;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface ApplicationRepository extends MongoRepository<Application, String> {
-    List<Application> findByOrganizerIdOrderByCreatedAtDesc(String organizerId);
-}
